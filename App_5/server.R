f4d831d462da8251471e9356d3ec44911877bd5e
mariakoziarz <maria.koziarz@gmail.com>
2024-05-09 17:15:45 +0200

    Final version

--- App_5/server.R
+++ App_5/server.R
@@ -2,22 +2,34 @@ library(shiny)
 library(ggplot2)
 library(tidyr)
 
+#load the data
 genes<-read.csv("TPMs_table_100genes.csv")
+
+#make it long
 genes_long<-pivot_longer(data=genes,
                          cols=colnames(genes)[-1],
-                         values_to = "TPMs",
+                         values_to = "Expression",
                          names_to = "Sample")
 
-shinyServer(function(input, output, session){
+#define function to select data and prepare a plot
+function(input, output, session){
   
-  updateSelectInput(session, "gene", choices=genes_long$GeneID)
+  #update gene selection choices with default option "--gene--"
+  updateSelectInput(session, "gene", choices=c("--gene--", unique(genes_long$GeneID)), selected = "--gene--")
   
-  
+  #create a plot based on the selected gene
   output$geneExpressionPlot<-renderPlot({
     
-    gene_epression<-genes_long[genes_long$GeneID==input$gene, ]
-    
-    ggplot(gene_expression, aes(x=Sample,y=TPMs) )+
-      geom_violin(color="black", aes(fill=Expression))
+    #if the gene not selected do not show a plot
+    if (input$gene == "--gene--") {
+      return(NULL)
+    }
+    #get the data for selected gene
+    gene_expression<-genes_long[genes_long$GeneID==input$gene, ]
+   
+    #create scatter plot
+    ggplot(gene_expression, aes(x=Sample,y=Expression, fill=Sample) )+
+      geom_point(aes(color=Sample))+
+      ylim(0,max(genes_long$Expression))
   })
-})
+}
--- App_5/ui.R
+++ App_5/ui.R
@@ -1,22 +1,30 @@
 library(shiny)
+library(ggplot2)
+library(tidyr)
 
+#load the data
+genes<-read.csv("TPMs_table_100genes.csv")
 
+#make it long
+genes_long<-pivot_longer(data=genes,
+                         cols=colnames(genes)[-1],
+                         values_to = "Expression",
+                         names_to = "Sample")
+
+#define ui for the aplication
 shinyUI(fluidPage(
   
- 
+  #add the title
   titlePanel("Gene Expression Viewer"),
   
-  
+  #create side bar for input gene selection
   sidebarLayout(
     sidebarPanel(
-      selectInput("gene", "Select Gene:", choices = NULL)
+      selectInput("gene", "Select Gene:", choices = genes_long$GeneID)
     ),
     
-    fluidRow(
-      column(width=8,
-        plotOutput("geneExpressonPlot")
-      )
-    )
+    #display the plot
+    mainPanel(plotOutput("geneExpressionPlot")    )
     
   )
 ))
